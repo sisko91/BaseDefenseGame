492236bd9744d36a87ea081ec1b93d1cdb7b50b3
Mike <mseanmayo@gmail.com>
2025-02-27 13:02:58 -0800

    Define world dimensions on world.tscn.
    World.RegionBounds now defines the size of the world (currently only
    impacts background texture rendering).
    
    This can be set on the world template (world.tscn) or on the main
    scene's instantiated World node.

--- World.cs
+++ World.cs
@@ -1,15 +1,29 @@
 using Godot;
 using System;
 
-public partial class World : Node
+public partial class World : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // The extents / bounds of the world, in world-space units. X is the width, Y is the height. The world extends half of this distance in each direction from the World's location.
+    [Export]
+    public Vector2 RegionBounds { get; set; }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Cached reference to the background sprite defined by world.tscn.
+    private Sprite2D backgroundSprite;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        backgroundSprite = GetNode<Sprite2D>("Background");
+        // We enable Regions + Tiling and stretch the background to cover the world RegionBounds.
+        backgroundSprite.RegionEnabled = true;
+        backgroundSprite.TextureRepeat = TextureRepeatEnum.Enabled;
+        var rect = backgroundSprite.RegionRect;
+        rect.Size = RegionBounds;
+        backgroundSprite.RegionRect = rect;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
