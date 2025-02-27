719f5dcd497ec08153de996ad999a7dac493d05c
Mike <mseanmayo@gmail.com>
2025-02-27 10:46:30 -0800

    Add empty Player scene, instantiate on main start.
    Added player.tscn (backed by Player.cs).
    
    Main.cs exports a PackedScene to configure the Player scene to use.
    Main.cs then instantiates the Player from this template and adds it as a
    child of the World scene.

--- Main.cs
+++ Main.cs
@@ -3,13 +3,27 @@ using System;
 
 public partial class Main : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // Cached reference to the world node instantiated as part of the main.tscn.
+    private World world;
+    // Cached reference to the player node instantiated below during scene startup.
+    private Player player;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Configurable template for the player to instantiate within the world during startup.
+    [Export]
+    public PackedScene PlayerTemplate {  get; private set; }
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        world = GetNode<World>("World");
+
+        player = PlayerTemplate.Instantiate<Player>();
+        player.Name = "Player";
+        world.AddChild(player);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
