b178dc79b808238d12ea0dfb38e2e5d7c5c6d17b
Mike <mseanmayo@gmail.com>
2025-02-27 11:42:59 -0800

    Add a Player camera; Support zooming.
    Player owns the camera so the scene now appears centered on the player
    (despite them still spawning at 0,0).
    
    Player camera supports zooming again. I factored the framerate into the
    calculations here so that zoom speed is stable regardless of
    platform/performance since it was easy (and a habit we'll want to
    build).
    
    It's debatable if the player should own the camera's configuration or
    not. I suspect in time we'll want our own dedicated Camera type where
    the config could live eventually.

--- Player.cs
+++ Player.cs
@@ -3,13 +3,54 @@ using System;
 
 public partial class Player : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // How fast the camera should zoom in or out. At 1.0, the camera zooms by a factor of 100% per /second/ of continuous zoom input.
+    [Export]
+    public float CameraZoomRate = 10.0f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // The largest camera zoom factor permitted. At 2.0, the camera is zoomed in 2x (from default).
+    [Export]
+    public float CameraZoomMax = 2.0f;
+
+    // The smallest camera zoom factor permitted. At 0.5, the camrea is zoomed out 2x (from default).
+    [Export]
+    public float CameraZoomMin = 0.5f;
+
+    // Cached camera reference from the player.tscn.
+    public Camera2D Camera { get; private set; }
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Camera = GetNode<Camera2D>("Camera2D");
+
+        if(CameraZoomRate <= 0)
+        {
+            CameraZoomRate = 1.0f;
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        HandleZoom(delta);
+    }
+
+    private void HandleZoom(double delta)
+    {
+        // Apply a fraction of total zoom based on the frame time.
+        float zoomIncrement = CameraZoomRate * (float)delta;
+        var zoomIncrementVec = new Vector2(zoomIncrement, zoomIncrement);
+        if (Input.IsActionJustPressed("camera_zoom_in"))
+        {
+            GD.Print("Zoom In: " + Camera.Zoom);
+            Camera.Zoom += zoomIncrementVec;
+        }
+        else if (Input.IsActionJustPressed("camera_zoom_out"))
+        {
+            GD.Print("Zoom Out: " + Camera.Zoom);
+            Camera.Zoom -= zoomIncrementVec;
+        }
+        // Enforce min/max zoom.
+        Camera.Zoom = Camera.Zoom.Clamp(new Vector2(CameraZoomMin, CameraZoomMin), Vector2.One * CameraZoomMax);
+    }
 }
