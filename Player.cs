cb58aebb43de09ca28c4c5016a28b0e8c5fa2930
Mike <mseanmayo@gmail.com>
2025-02-27 13:46:13 -0800

    Add custom camera class used by player.tscn.
    PlayerCamera now encapsulates the configuration and input processing for
    zooming the camera in and out. Player now just holds a reference to the
    PlayerCamera.
    
    There is no dedicated scene for the PlayerCamera since there is no data
    associated with it (yet). For now, player.tscn's Camera2D node simply
    has PlayerCamera.cs assigned as its script reference.

--- Player.cs
+++ Player.cs
@@ -3,34 +3,17 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-    // How fast the camera should zoom in or out. At 1.0, the camera zooms by a factor of 100% per /second/ of continuous zoom input.
-    [Export]
-    public float CameraZoomRate = 10.0f;
-
-    // The largest camera zoom factor permitted. At 2.0, the camera is zoomed in 2x (from default).
-    [Export]
-    public float CameraZoomMax = 2.0f;
-
-    // The smallest camera zoom factor permitted. At 0.5, the camrea is zoomed out 2x (from default).
-    [Export]
-    public float CameraZoomMin = 0.5f;
-
     // How fast the player can move in any direction.
     [Export]
     public float MovementSpeed = 400.0f;
 
     // Cached camera reference from the player.tscn.
-    public Camera2D Camera { get; private set; }
+    public PlayerCamera Camera { get; private set; }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        Camera = GetNode<Camera2D>("Camera2D");
-
-        if(CameraZoomRate <= 0)
-        {
-            CameraZoomRate = 1.0f;
-        }
+        Camera = GetNode<PlayerCamera>("Camera2D");
     }
 
     // Called every tick of the physics thread.
@@ -57,28 +40,5 @@ public partial class Player : CharacterBody2D
     {
         // For now we just default to sliding along surfaces the player collides with.
         Velocity = Velocity.Slide(collision.GetNormal());
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        HandleZoom(delta);
-    }
-
-    private void HandleZoom(double delta)
-    {
-        // Apply a fraction of total zoom based on the frame time.
-        float zoomIncrement = CameraZoomRate * (float)delta;
-        var zoomIncrementVec = new Vector2(zoomIncrement, zoomIncrement);
-        if (Input.IsActionJustPressed("camera_zoom_in"))
-        {
-            Camera.Zoom += zoomIncrementVec;
-        }
-        else if (Input.IsActionJustPressed("camera_zoom_out"))
-        {
-            Camera.Zoom -= zoomIncrementVec;
-        }
-        // Enforce min/max zoom.
-        Camera.Zoom = Camera.Zoom.Clamp(new Vector2(CameraZoomMin, CameraZoomMin), Vector2.One * CameraZoomMax);
     }
 }
---
+++ PlayerCamera.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public partial class PlayerCamera : Camera2D
+{
+    // How fast the camera should zoom in or out. At 1.0, the camera zooms by a factor of 100% per /second/ of continuous zoom input.
+    [Export]
+    public float CameraZoomRate = 10.0f;
+
+    // The largest camera zoom factor permitted. At 2.0, the camera is zoomed in 2x (from default).
+    [Export]
+    public float CameraZoomMax = 2.0f;
+
+    // The smallest camera zoom factor permitted. At 0.5, the camrea is zoomed out 2x (from default).
+    [Export]
+    public float CameraZoomMin = 0.5f;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        if (CameraZoomRate <= 0)
+        {
+            CameraZoomRate = 1.0f;
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        HandleZoom(delta);
+    }
+
+    private void HandleZoom(double delta)
+    {
+        // Apply a fraction of total zoom based on the frame time.
+        float zoomIncrement = CameraZoomRate * (float)delta;
+        var zoomIncrementVec = new Vector2(zoomIncrement, zoomIncrement);
+        if (Input.IsActionJustPressed("camera_zoom_in"))
+        {
+            Zoom += zoomIncrementVec;
+        }
+        else if (Input.IsActionJustPressed("camera_zoom_out"))
+        {
+            Zoom -= zoomIncrementVec;
+        }
+        // Enforce min/max zoom.
+        Zoom = Zoom.Clamp(new Vector2(CameraZoomMin, CameraZoomMin), Vector2.One * CameraZoomMax);
+    }
+}
