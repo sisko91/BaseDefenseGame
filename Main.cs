a93504df1d3bc827cd513c7c334e0d4b5fee53c3
Mike <mseanmayo@gmail.com>
2025-03-03 15:15:54 -0800

    Make escape quit the game.

--- Main.cs
+++ Main.cs
@@ -36,5 +36,9 @@ public partial class Main : Node
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        if(Input.IsActionJustPressed("debug_quit"))
+        {
+            GetTree().Quit();
+        }
     }
 }
