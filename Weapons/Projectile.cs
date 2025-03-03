9bb2a2ce292b7fb4ffbbedb665304435f1063e23
Mike <mseanmayo@gmail.com>
2025-03-03 12:31:33 -0800

    Add Scattergun weapon.
    Not very different from pistol yet but it fires multiple projectiles and
    those projectiles are slightly different from pistol bullets.
    
    Also added a new Projectile type currently only used for Scattergun.
    
    SG's projectiles are NOT interactive with Physics 2D Layer Mask #2
    (other projectiles). Since they all fire from the same barrel these were
    colliding with each other and doing silly things. We can add this
    capability back intentionally if we want, eventually.

---
+++ Weapons/Projectile.cs
@@ -0,0 +1,41 @@
+using ExtensionMethods;
+using Godot;
+using System;
+
+public partial class Projectile : CharacterBody2D
+{
+    [Export]
+    public float InitialSpeed = 750.0f;
+
+    [Export]
+    public float LifetimeSeconds = 3.0f;
+
+    [Export]
+    public float Damage = 20.0f;
+
+    // Adds the projectile to the gameworld and initializes its position, velocity, etc.
+    public void Start(Vector2 worldPosition, float worldDirection)
+    {
+        GlobalRotation = worldDirection;
+        GlobalPosition = worldPosition;
+        Velocity = new Vector2(InitialSpeed, 0).Rotated(GlobalRotation);
+
+        var timer = this.GetGameWorld().GetTree().CreateTimer(LifetimeSeconds);
+        timer.Timeout += QueueFree;
+
+        this.GetGameWorld().AddChild(this);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var collision = MoveAndCollide(Velocity * (float)delta);
+        if (collision != null)
+        {
+            if (collision.GetCollider() is NonPlayerCharacter npc)
+            {
+                npc.ReceiveHit(collision, this, Damage);
+            }
+            QueueFree();
+        }
+    }
+}
---
+++ Weapons/Scattergun/Scattergun.cs
@@ -0,0 +1,43 @@
+using ExtensionMethods;
+using Godot;
+using System;
+
+public partial class Scattergun : Weapon
+{
+    [Export]
+    public PackedScene RoundTemplate = null;
+
+    [Export]
+    public uint RoundsPerFire = 4;
+
+    // Maximum angle - in degrees - that any round within a fired payload may deviate from straight ahead (in either direction).
+    [Export]
+    public float MaxRoundSpreadDegrees = 20.0f;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+    public override void PressFire()
+    {
+        var roundSpreadRads = Mathf.DegToRad(Mathf.Abs(MaxRoundSpreadDegrees));
+        for (int i = 0; i < RoundsPerFire; i++)
+        {
+            var round = RoundTemplate.Instantiate<Projectile>();
+            var randNegativeOneToOne = (GD.Randf() - 0.5f) * 2;
+            float rotationOffset = roundSpreadRads * randNegativeOneToOne;
+            round.Start(GlobalPosition, GlobalRotation + rotationOffset);
+        }
+    }
+
+    public override void ReleaseFire()
+    {
+       
+    }
+}
