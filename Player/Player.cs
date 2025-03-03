d2d58652af93b60fd98a718d85a29e00fecd63b4
Mike <mseanmayo@gmail.com>
2025-03-03 13:34:08 -0800

    Allow player to equip + cycle multiple weapons.
    Expanded StarterWeaponTemplates to be an array on the player scene.
    Player is now granted both Pistol and Scattergun.
    
    Weapons can be cycled freely with keyboard (PgUp / PgDown) or Gamepad
    (D-Pad Right/Left).

--- Player/Player.cs
+++ Player/Player.cs
@@ -9,15 +9,48 @@ public partial class Player : CharacterBody2D
     [Export]
     public float MovementSpeed = 400.0f;
 
-    // The default / starter weapon that the player always spawns with. May be null.
-    [Export]
-    public PackedScene StarterWeaponTemplate = null;
-
     // Cached camera reference from the player.tscn.
     public PlayerCamera Camera { get; private set; }
 
+    #region Weapons
+
+    // The default / starter weapons that the player always spawns with. May be empty.
+    // The first template in the array will be the weapon initially equipped to the player's weapon ring.
+    [Export]
+    public Godot.Collections.Array<PackedScene> StarterWeaponTemplates;
+
     // Cached weapon ring reference from the player.tscn.
     public WeaponRing WeaponRing { get; private set; }
+
+    // Runtime list of weapon instances the player owns.
+    public Godot.Collections.Array<Weapon> Weapons { get; private set; }
+    
+    protected Weapon EquippedWeapon { get { return WeaponRing.EquippedWeapon; } }
+    protected Weapon NextWeapon { 
+        get 
+        {
+            var idx = Weapons.IndexOf(EquippedWeapon) + 1;
+            if(idx >= Weapons.Count)
+            {
+                idx = 0;
+            }
+            return Weapons[idx];
+        } 
+    }
+    protected Weapon PreviousWeapon
+    {
+        get
+        {
+            var idx = Weapons.IndexOf(EquippedWeapon) - 1;
+            if(idx < 0)
+            {
+                idx = Weapons.Count - 1;
+            }
+            return Weapons[idx];
+        }
+    }
+
+    #endregion Weapons
 
     // Whether the player is using a gamepad (when false, keyboard+mouse is assumed).
     public bool bUsingGamepad { get; private set; } = false;
@@ -30,10 +63,16 @@ public partial class Player : CharacterBody2D
         Camera = GetNode<PlayerCamera>("Camera2D");
         WeaponRing = GetNode<WeaponRing>("WeaponRing");
 
-        if(StarterWeaponTemplate != null)
+        Weapons = new Godot.Collections.Array<Weapon>();
+
+        foreach(var starterWeaponTemplate in StarterWeaponTemplates)
         {
-            var starterWeapon = StarterWeaponTemplate.Instantiate<Weapon>();
-            WeaponRing.Equip(starterWeapon);
+            var starterWeapon = starterWeaponTemplate.Instantiate<Weapon>();
+            Weapons.Add(starterWeapon);
+            if(WeaponRing.EquippedWeapon == null)
+            {
+                WeaponRing.Equip(starterWeapon);
+            }
         }
     }
 
@@ -110,11 +149,11 @@ public partial class Player : CharacterBody2D
 
     private void HandleAction(double delta) {
         if (Input.IsActionJustPressed("shoot")) {
-            WeaponRing.EquippedWeapon.PressFire();
+            WeaponRing.EquippedWeapon?.PressFire();
         }
         else if(Input.IsActionJustReleased("shoot"))
         {
-            WeaponRing.EquippedWeapon.ReleaseFire();
+            WeaponRing.EquippedWeapon?.ReleaseFire();
         }
 
         if (Input.IsActionJustPressed("throw_grenade"))
@@ -132,6 +171,15 @@ public partial class Player : CharacterBody2D
             timer.Timeout += grenade.Explode;
 
             this.GetGameWorld().AddChild(grenade);
+        }
+
+        if(Input.IsActionJustPressed("cycle_weapon_forward"))
+        {
+            WeaponRing.Equip(NextWeapon);
+        }
+        else if (Input.IsActionJustPressed("cycle_weapon_back"))
+        {
+            WeaponRing.Equip(PreviousWeapon);
         }
     }
 }
