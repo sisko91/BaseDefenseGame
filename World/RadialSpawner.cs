72fdcf4132fadcf704f3cfa1ac5f3a49d10e0f5c
Mike <mseanmayo@gmail.com>
2025-03-14 14:21:51 -0700

    Support sub-360 arcs for RadialSpawner.
    Also repositioned this above the buildings and only in a half-circle
    now. The problem was that when the buildings overlapped the spawn area
    enemies would spawn behind the building and none of their collision
    worked correctly.
    
    There might be more to fix there, but this at least gives us more
    flexibility in where/how we place enemies in the world.

--- World/RadialSpawner.cs
+++ World/RadialSpawner.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 // Radial Spawner spawns the specified template at locations within a radius from its central position.
 [Tool] // Enables the script to run in the editor
@@ -35,6 +34,32 @@ public partial class RadialSpawner : Node2D
     }
     private float _outerRadius = 10;
 
+    // The beginning angle of the radial arc. When this and StopAngleDegrees are 0 and 360 respectively, a full circle is used for the spawn regions.
+    [Export]
+    public float StartAngleDegrees
+    {
+        get => _startAngleDegrees;
+        set
+        {
+            _startAngleDegrees = value;
+            QueueRedraw();
+        }
+    }
+    private float _startAngleDegrees = 0;
+
+    // The terminating angle of the radial arc. When the delta between this and StartAngleDegrees is >= 360, a full circle is used for the spawn regions.
+    [Export]
+    public float StopAngleDegrees
+    {
+        get => _stopAngleDegrees;
+        set
+        {
+            _stopAngleDegrees = value;
+            QueueRedraw();
+        }
+    }
+    private float _stopAngleDegrees = 360;
+
     // How many instances should be spawned during _Ready().
     [Export]
     public uint InitialCount { get; set; } = 1;
@@ -53,7 +78,8 @@ public partial class RadialSpawner : Node2D
 
         for (uint i = 0; i < InitialCount; i++)
         {
-            SpawnRandom();
+            // Certain things (like the debug renderers) can't be invoked during _Ready().
+            Callable.From(SpawnRandom).CallDeferred();
         }
     }
 
@@ -66,16 +92,18 @@ public partial class RadialSpawner : Node2D
         }
 
         var instance = SpawnTemplate.Instantiate<Node2D>();
+        AddChild(instance);
 
-        var randomAngle = GD.Randf() * Mathf.Pi * 2;
+        // Calculate the angle as StartAngleDegrees + random % of the arc
+        var angleRange = StopAngleDegrees - StartAngleDegrees;
+        var randomAngleDegrees = StartAngleDegrees + angleRange * GD.Randf() - 90.0f;
+
         var randomDistance = InnerRadius + GD.Randf() * (OuterRadius - InnerRadius);
-        instance.Position = Position + Vector2.FromAngle(randomAngle) * randomDistance;
-
+        instance.GlobalPosition = GlobalPosition + Vector2.FromAngle(Mathf.DegToRad(randomAngleDegrees)) * randomDistance;
+        
         if (GroupName != null && GroupName != "") {
             instance.AddToGroup(GroupName);
         }
-
-        AddChild(instance);
 
         return instance;
     }
@@ -85,7 +113,34 @@ public partial class RadialSpawner : Node2D
         if (!Engine.IsEditorHint()) return; // Only draw in the editor
 
         // Draw a circle in the editor
-        DrawCircle(Vector2.Zero, InnerRadius, new Color(0, 0, 1, 0.5f)); // Blue with transparency
-        DrawCircle(Vector2.Zero, OuterRadius, new Color(1, 0, 0, 0.5f)); // Red with transparency
+        float arcAngle = Mathf.Abs(StopAngleDegrees - StartAngleDegrees);
+        if(arcAngle >= 360)
+        {
+            DrawCircle(Vector2.Zero, InnerRadius, new Color(0, 0, 1, 0.5f)); // Blue with transparency
+            DrawCircle(Vector2.Zero, OuterRadius, new Color(1, 0, 0, 0.5f)); // Red with transparency
+        }
+        else
+        {
+            DrawArc(Vector2.Zero, InnerRadius, StartAngleDegrees, StopAngleDegrees, new Color(0, 0, 1, 0.5f)); // Blue with transparency
+            DrawArc(Vector2.Zero, OuterRadius, StartAngleDegrees, StopAngleDegrees, new Color(1, 0, 0, 0.5f)); // Red with transparency
+        }
+    }
+
+    // Adapted from https://docs.godotengine.org/en/4.0/tutorials/2d/custom_drawing_in_2d.html#an-example-drawing-circular-arcs
+    // TODO: Move this somewhere more global?
+    private void DrawArc(Vector2 center, float radius, float angleFromDegrees, float angleToDegrees, Color color)
+    {
+        int nbPoints = 32;
+        var pointsArc = new Vector2[nbPoints + 2];
+        pointsArc[0] = center;
+        var colors = new Color[] { color };
+
+        for (int i = 0; i <= nbPoints; i++)
+        {
+            float anglePoint = Mathf.DegToRad(angleFromDegrees + i * (angleToDegrees - angleFromDegrees) / nbPoints - 90);
+            pointsArc[i + 1] = center + new Vector2(Mathf.Cos(anglePoint), Mathf.Sin(anglePoint)) * radius;
+        }
+
+        DrawPolygon(pointsArc, colors);
     }
 }
