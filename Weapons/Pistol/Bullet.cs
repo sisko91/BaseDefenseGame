6c28d822c85baf8bd120db219ec9f6b79ab25f07
Mike <mseanmayo@gmail.com>
2025-03-15 14:21:02 -0700

    Minor bullet improvements.
    Bullets never bounce off of any kind of character so I simplified this
    check.
    
    Bullet scale was weird in the scene, too.

--- Weapons/Pistol/Bullet.cs
+++ Weapons/Pistol/Bullet.cs
@@ -6,7 +6,7 @@ public partial class Bullet : Projectile
     protected override void OnCollide(KinematicCollision2D collision)
     {
         // Make bullets bounce off walls (really anything that isn't a player, NPC, etc.)
-        bool bMightBeAWall = !(collision.GetCollider() is Player or NonPlayerCharacter or Projectile);
+        bool bMightBeAWall = !(collision.GetCollider() is Character or Projectile);
         if (bMightBeAWall)
         {
             Velocity = Velocity.Bounce(collision.GetNormal());
