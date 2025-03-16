201f3c5b83ba50e4ebde4eed718d3d45ed1ae298
sisko91 <86699085+sisko91@users.noreply.github.com>
2025-03-16 00:20:04 -0700

    Day night config
    Increased day length to 5 minutes and default to start at noon

--- World/DayNight/DayNight.cs
+++ World/DayNight/DayNight.cs
@@ -9,7 +9,10 @@ public partial class DayNight : Node2D
     Timer dayTimer;
 
     [Export]
-    public int dayLengthSeconds = 15;
+    public int dayLengthSeconds = 300;
+
+    [Export]
+    public float dayStartTime = 0.5f; //0 = midnight, 0.5 = noon, 1 = midnight
     public override void _Ready()
     {
         lightGradient = GD.Load<GradientTexture2D>("res://art/World/daynight.tres");
@@ -23,6 +26,7 @@ public partial class DayNight : Node2D
     public override void _Process(double delta)
     {
         var dayPercent = 1 - dayTimer.TimeLeft / dayTimer.WaitTime;
+        dayPercent = (dayPercent + dayStartTime) % 1;
         var dayColor = lightGradient.Gradient.Sample((float)dayPercent);
         if (Material != null) //Dunno how this is null sometimes
         {
