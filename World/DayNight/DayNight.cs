f684a62f029d214d8d7e4f53bf9facd7bca8c8c6
sisko91 <chris.hudson.m@gmail.com>
2025-04-02 00:49:06 -0700

    Add config to pause time of day

--- World/DayNight/DayNight.cs
+++ World/DayNight/DayNight.cs
@@ -5,35 +5,45 @@ using System;
 public partial class DayNight : Node
 {
 
-    GradientTexture2D lightGradient;
-    Timer dayTimer;
+    GradientTexture2D LightGradient;
+    Timer DayTimer;
 
     [Export]
-    public int dayLengthSeconds = 300;
+    public int DayLengthSeconds = 300;
 
     [Export]
-    public float dayStartTime = 0.5f; //0 = midnight, 0.5 = noon, 1 = midnight
+    public float DayStartTime = 0.5f; //0 = midnight, 0.5 = noon, 1 = midnight
 
-    public Color dayNightColor { get; private set; }
+    [Export]
+    public bool FreezeTime = false;
+
+    public Color DayNightColor { get; private set; }
 
     public override void _Ready()
     {
-        lightGradient = GD.Load<GradientTexture2D>("res://art/World/daynight.tres");
+        LightGradient = GD.Load<GradientTexture2D>("res://art/World/daynight.tres");
 
-        dayTimer = new Timer();
-        dayTimer.WaitTime = dayLengthSeconds;
-        dayTimer.Autostart = true;
-        AddChild(dayTimer);
+        DayTimer = new Timer();
+        DayTimer.WaitTime = DayLengthSeconds;
+        DayTimer.Autostart = true;
+        AddChild(DayTimer);
 
-        dayNightColor = new Color(0, 0, 0, 0);
+        DayNightColor = new Color(0, 0, 0, 0);
     }
 
     public override void _Process(double delta)
     {
-        var dayPercent = 1 - dayTimer.TimeLeft / dayTimer.WaitTime;
-        dayPercent = (dayPercent + dayStartTime) % 1;
-        dayNightColor = lightGradient.Gradient.Sample((float)dayPercent);
+        if (FreezeTime) {
+            DayTimer.Paused = true;
+            return;
+        } else if (DayTimer.Paused) {
+            DayTimer.Paused = false;
+        }
 
-        RenderingServer.GlobalShaderParameterSet("day_night_color", dayNightColor);
+        var dayPercent = 1 - DayTimer.TimeLeft / DayTimer.WaitTime;
+        dayPercent = (dayPercent + DayStartTime) % 1;
+        DayNightColor = LightGradient.Gradient.Sample((float)dayPercent);
+
+        RenderingServer.GlobalShaderParameterSet("day_night_color", DayNightColor);
     }
 }
