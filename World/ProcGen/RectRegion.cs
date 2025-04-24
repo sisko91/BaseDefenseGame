0719a22e2af3486a6224f15652089137fcf1b8fc
Mike <mseanmayo@gmail.com>
2025-04-24 12:32:58 -0700

    Make RectRegion drag-resizable in editor.
    This kinda got crazy but I wanted to see it through once I realized it
    worked:
    
    RectRegion is a [Tool] so that it runs in the editor, but most/all of
    its functionality ONLY runs in the editor and all it does is expose a
    `Rect2` as a draggable, transformable node.
    
    RectRegion is NOT an EditorPlugin. It uses Godot 4+ features to remain a
    gameplay component marked as [Tool] while also using the EditorInterface
    directly to manipulate selections and input handling in the editor.
    
    At a high level, this works by carefully controlling what nodes are
    selected in the editor when input is detected. The secret sauce is
    quietly enabling `InternalProcess` notifications for this node, which
    DOESN'T cause _Process() to be invoked but DOES cause the notification
    to pass through to _Notification() (where this handles input and draw
    calls).

--- World/ProcGen/RectRegion.cs
+++ World/ProcGen/RectRegion.cs
@@ -6,26 +6,102 @@ using System;
 [Tool]
 public sealed partial class RectRegion : Node2D
 {
-    private Rect2 _region;
-
     [Export]
-    public Rect2 Region {
-        get => _region;
-        set {
-            _region = value;
-            QueueRedraw();
-        }
-    }
+    public Rect2 Region { get; set; }
+
+    [ExportCategory("Editor View")]
+    [Export]
+    public Color EditorDrawColor { get; private set; } = Colors.BlueViolet;
+
+    private bool isDragging = false;
+    private Vector2 dragAnchorOffset = Vector2.Zero;
 
     public override void _Ready() {
         base._Ready();
+
+        if(Engine.IsEditorHint()) {
+            // This doesn't enable _Process() in the editor for this node, but it DOES cause _Notification() to fire for that event.
+            SetProcessInternal(true);
+        }
+    }
+
+    public override void _Notification(int what) {
+        base._Notification(what);
+
+        if(!Engine.IsEditorHint()) {
+            return;
+        }
+
+        if (what == NotificationInternalProcess) {
+            QueueRedraw();
+
+            bool isMousePressed = Input.IsMouseButtonPressed(MouseButton.Left);
+            if(isMousePressed) {
+                if(!isDragging) {
+                    Vector2 mousePos = EditorInterface.Singleton.GetEditorViewport2D().GetMousePosition();
+                    if (IsMouseOverHandle(mousePos)) {
+                        GD.Print("Drag-resize started.");
+                        isDragging = true;
+                        dragAnchorOffset = Region.End - mousePos;
+                        // We don't want to select ANYTHING when we're dragging around, so wipe the editor's selection list clean.
+                        EditorInterface.Singleton.GetSelection().Clear();
+                    }
+                }
+            }
+            else {
+                if(isDragging) {
+                    GD.Print("Drag-resize completed.");
+                    isDragging = false;
+                    // We want the region to be re-selected (and nothing else) after we're done dragging around.
+                    EditorInterface.Singleton.GetSelection().Clear();
+                    EditorInterface.Singleton.GetSelection().AddNode(this);
+                }
+            }
+
+            if(isDragging) {
+                Vector2 mousePos = EditorInterface.Singleton.GetEditorViewport2D().GetMousePosition();
+                var newSize = mousePos + dragAnchorOffset - Region.Position;
+                Region = new Rect2(Region.Position, newSize);
+            }
+        }
     }
 
     public override void _Draw() {
-        if(Engine.IsEditorHint()) {
-            // Partially-transparent
-            var drawColor = new Color(0, 0.5f, 0.5f, 0.5f);
-            DrawRect(rect: Region, color: drawColor, filled: true);
+        if(!Engine.IsEditorHint()) {
+            return;
         }
+
+        if (isDragging || IsSelectedInEditor()) {
+            // Always half-transparent.
+            var drawColor = EditorDrawColor;
+            drawColor.A = 0.5f;
+            DrawRect(rect: Region, color: drawColor, filled: true);
+
+            // Early-exit from rendering control points at all if the region is very small.
+            if (Region.Size.LengthSquared() <= 10.0f) {
+                return;
+            }
+
+            var zoom = EditorInterface.Singleton.GetEditorViewport2D().GlobalCanvasTransform.Scale;
+            DrawCircle(Region.End, 5.0f / zoom.X, Colors.White, filled: true);
+        }
+    }
+
+    // Returns true if this node is currently selected in the editor.
+    private bool IsSelectedInEditor() {
+        if(Engine.IsEditorHint()) {
+            var selection = EditorInterface.Singleton.GetSelection();
+            return selection?.GetSelectedNodes().Contains(this) ?? false;
+        }
+        return false;
+    }
+
+    private bool IsMouseOverHandle(Vector2 mousePos) {
+        if(Engine.IsEditorHint()) {
+            var zoom = EditorInterface.Singleton.GetEditorViewport2D().GlobalCanvasTransform.Scale;
+            float handleRadius = 5.0f / zoom.X;
+            return Region.End.DistanceTo(mousePos) <= handleRadius;
+        }
+        return false;
     }
 }
