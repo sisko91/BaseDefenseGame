8dfce1504142e5ecac8d852b04fa4f05d2720ed2
Mike <mseanmayo@gmail.com>
2025-05-12 13:54:27 -0700

    Cache Placeable footprints for performance.
    Thinking on the last commit a bit more, I don't see a reason for this
    state to change during gameplay. We set it up during design in the
    editor and then we access it through these getters, so if that's the
    full contract there's no reason these can't be cached.
    
    Note regarding the previous commit: I experimented with this change +
    reverting the optimization in the last commit. This change to
    Placeable.cs on its own brings SmallTown generation times down from ~6
    seconds to ~2 seconds. But the optimization in the last commit brings
    timings down to ~540ms. So this is more of a general improvement whereas
    the last commit was a targeted improvement.

--- World/ProcGen/Placeable.cs
+++ World/ProcGen/Placeable.cs
@@ -12,11 +12,33 @@ namespace Gurdy.ProcGen
         // PlacedFootprint is used to determine overlaps and constraints such as overlaps and out of bounds locations where this scene cannot be placed
         // by a ProcGen routine. The footprint can be offset and smaller/larger than the actual Placeable scene to finely control placement logic
         // depending on where the origin of the Placeable scene and other constituents are.
-        public RectRegion PlacedFootprint => GetNode<RectRegion>("PlacedFootprint");
+        public RectRegion PlacedFootprint
+        {
+            get
+            {
+                if (_placedFootprint == null)
+                {
+                    _placedFootprint = GetNode<RectRegion>("PlacedFootprint");
+                }
+                return _placedFootprint;
+            }
+        }
+        private RectRegion _placedFootprint = null;
 
-        // A dynamically-generated list of all RectRegions parented under /SecondaryFootprints on the Placeable scene.
-        // These are inspected during ProcGen routines.
-        public IEnumerable<RectRegion> SecondaryFootprints => GetNodeOrNull("SecondaryFootprints")?.GetChildren().OfType<RectRegion>() ?? [];
+        // A list of all RectRegions parented under /SecondaryFootprints on the Placeable scene.
+        // These are inspected during ProcGen routines. This operation is cached once it is called for the first time.
+        public IEnumerable<RectRegion> SecondaryFootprints
+        {
+            get
+            {
+                if (_secondaryFootprints == null)
+                {
+                    _secondaryFootprints = GetNodeOrNull("SecondaryFootprints")?.GetChildren().OfType<RectRegion>().ToList() ?? [];
+                }
+                return _secondaryFootprints;
+            }
+        }
+        private List<RectRegion> _secondaryFootprints = null;
 
         public override void _Ready()
         {
